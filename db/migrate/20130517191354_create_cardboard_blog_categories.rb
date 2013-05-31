269bb17de3a61a3de0cd9565b05e826d5236a4a4
Leonel Galan <leonel@smashingboxes.com>
2013-05-31 15:38:20 -0400

    No need for migrations in gem, copied to project using generator instead

--- db/migrate/20130517191354_create_cardboard_blog_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateCardboardBlogCategories < ActiveRecord::Migration
-  def change
-    create_table :cardboard_blog_categories do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20130517191849_category_post.rb
+++
@@ -1,8 +0,0 @@
-class CategoryPost < ActiveRecord::Migration
-  create_table :categories_posts, id: false do |t|
-    t.references :category
-    t.references :post
-  end
-  add_index :categories_posts, [:category_id, :post_id]
-  add_index :categories_posts, [:post_id, :category_id]
-end
